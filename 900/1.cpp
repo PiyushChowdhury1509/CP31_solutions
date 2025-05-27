9968f2caeb7e164877128d175203e7632faed219
Piyush Chowdhury <piyushchowdhury1509@gmail.com>
2025-05-27 15:08:54 +0530

    solved 4 problems of 900

---
+++ 900/1.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <climits>
+#include <iomanip>
+#include <numeric>
+#include <cassert>
+#include <cstring>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define F first
+#define S second
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define repr(i, a, b) for (int i = a; i >= b; --i)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+struct pair_hash {
+    size_t operator()(const pair<int, int>& p) const {
+        return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
+    }
+};
+
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+ll mod_add(ll a, ll b, ll m = MOD) { return (a % m + b % m) % m; }
+ll mod_sub(ll a, ll b, ll m = MOD) { return ((a % m - b % m) + m) % m; }
+ll mod_mul(ll a, ll b, ll m = MOD) { return ((a % m) * (b % m)) % m; }
+ll mod_exp(ll a, ll b, ll m = MOD) {
+    ll res = 1;
+    a %= m;
+    while (b > 0) {
+        if (b & 1) res = (res * a) % m;
+        a = (a * a) % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main() {
+    fast_io;
+
+    int t ;  
+    cin>>t;
+    while (t--) {
+        int a,b,kx,ky,qx,qy;
+        cin>>a>>b>>kx>>ky>>qx>>qy;
+        vector<int>dx={1,1,-1,-1};
+        vector<int>dy={1,-1,1,-1};
+        unordered_set<pair<int,int>,pair_hash>king;
+        unordered_set<pair<int,int>,pair_hash>queen;
+        for(int i=0;i<4;i++){
+            int pointakx=kx+(dx[i]*a);
+            int pointaky=ky+(dy[i]*b);
+            int pointbkx=kx+(dx[i]*b);
+            int pointbky=ky+(dy[i]*a);
+            king.insert({pointakx,pointaky});
+            king.insert({pointbkx,pointbky});
+
+            int pointaqx=qx+(dx[i]*a);
+            int pointaqy=qy+(dy[i]*b);
+            int pointbqx=qx+(dx[i]*b);
+            int pointbqy=qy+(dy[i]*a);
+            queen.insert({pointaqx,pointaqy});
+            queen.insert({pointbqx,pointbqy});
+        }
+        int ans=0;
+        for(auto i:king){
+            if(queen.count(i)) ans++;
+        }
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
