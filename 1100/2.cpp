6f5fd021933244498797c9729ef8b25559a485fa
Piyush Chowdhury <piyushchowdhury1509@gmail.com>
2025-06-01 00:58:06 +0530

    solved second

---
+++ 1100/2.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <climits>
+#include <iomanip>
+#include <numeric>
+#include <cassert>
+#include <cstring>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define F first
+#define S second
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define repr(i, a, b) for (int i = a; i >= b; --i)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+ll mod_add(ll a, ll b, ll m = MOD) { return (a % m + b % m) % m; }
+ll mod_sub(ll a, ll b, ll m = MOD) { return ((a % m - b % m) + m) % m; }
+ll mod_mul(ll a, ll b, ll m = MOD) { return ((a % m) * (b % m)) % m; }
+ll mod_exp(ll a, ll b, ll m = MOD) {
+    ll res = 1;
+    a %= m;
+    while (b > 0) {
+        if (b & 1) res = (res * a) % m;
+        a = (a * a) % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main() {
+    fast_io;
+
+    int t ;
+    cin>>t;
+    while (t--) {
+        int n,k;
+        cin>>n>>k;
+        vector<int>a(n),b(n);
+        for(int i=0;i<n;i++) cin>>a[i];
+        for(int i=0;i<n;i++) cin>>b[i];
+        vector<int>v(n,0);
+        v[0]=b[0];
+        int maxm=b[0];
+        for(int i=1;i<n;i++){
+            a[i]+=a[i-1];
+            maxm=max(b[i],maxm);
+            v[i]=maxm;
+        }
+        int count=min(n,k);
+        int copy=count;
+        int ans=INT_MIN;
+        int idx=0;
+        while(copy){
+            int value=a[copy-1]+(k-copy)*v[copy-1];
+            ans=max(value,ans);
+            copy--;
+        }
+        
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
