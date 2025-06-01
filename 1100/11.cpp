5a3717ca276da52860e063180e16f38f152e7a89
Piyush Chowdhury <piyushchowdhury1509@gmail.com>
2025-06-01 13:12:04 +0530

    solved 7 and 11

---
+++ 1100/11.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <climits>
+#include <iomanip>
+#include <numeric>
+#include <cassert>
+#include <cstring>
+
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define pb push_back
+#define mp make_pair
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define F first
+#define S second
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+#define repr(i, a, b) for (int i = a; i >= b; --i)
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+ll mod_add(ll a, ll b, ll m = MOD) { return (a % m + b % m) % m; }
+ll mod_sub(ll a, ll b, ll m = MOD) { return ((a % m - b % m) + m) % m; }
+ll mod_mul(ll a, ll b, ll m = MOD) { return ((a % m) * (b % m)) % m; }
+ll mod_exp(ll a, ll b, ll m = MOD) {
+    ll res = 1;
+    a %= m;
+    while (b > 0) {
+        if (b & 1) res = (res * a) % m;
+        a = (a * a) % m;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main() {
+    fast_io;
+
+    int t ;
+    cin>>t;  
+    while (t--) {
+        int n,k;
+        cin>>n>>k;
+        vector<ll>nums(n);
+        ll sum=0;
+        for(int i=0;i<n;i++){
+            cin>>nums[i];
+            sum+=nums[i];
+        }
+        sort(all(nums));
+        vector<ll>pre(n,0),suff(n,0);
+        pre[0]=nums[0];
+        for(int i=1;i<n;i++){
+            pre[i]=pre[i-1]+nums[i];
+        }
+        suff[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--){
+            suff[i]=suff[i+1]+nums[i];
+        }
+        ll diff=min(pre[(2*k)-1],suff[n-k]);
+        int i=2*(k-1)-1;
+        int j=n-1;
+        while(i>0){
+            diff=min(diff,pre[i]+suff[j]);
+            i-=2;
+            j--;
+        }
+        cout<<sum-diff<<endl;
+    }
+
+    return 0;
+}
